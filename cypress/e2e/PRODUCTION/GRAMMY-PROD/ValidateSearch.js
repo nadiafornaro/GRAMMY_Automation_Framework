45fff7e45477dfede61dd89798c4e1a85a3038cf
nadiafornaro <nadymf@gmail.com>
2022-09-06 21:37:18 -0300

    Improving timeout on cypress

--- cypress/e2e/PRODUCTION/GRAMMY-PROD/ValidateSearch.js
+++ cypress/e2e/PRODUCTION/GRAMMY-PROD/ValidateSearch.js
@@ -91,6 +91,7 @@ describe('Validate Search Functionality in GRAMMY PROD', { tags: 'prod' }, funct
   artists.forEach(artist => {
     describe(`Starting search for : ${artist}`, () => {
       it('Validate search elements on search page ', function () {
+        defaultCommandTimeout: 20000
         cy.get('.form-node').type(artist)
         cy.wait(2000)
         cy.get('.max-w-960px > :nth-child(1) > .border-b').should('have.text', 'Artists')//validate artists column
