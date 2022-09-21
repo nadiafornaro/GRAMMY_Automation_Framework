37c7d4c23bb32f6c32e925a55583f0686d8e1191
wagnermetaltoad <wagner.lima@metaltoad.com>
2022-09-21 16:49:04 -0300

    TimeOut Load sites improvement

--- cypress.config.js
+++ cypress.config.js
@@ -32,5 +32,6 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    defaultCommandTimeout: 10000
   },
 })
