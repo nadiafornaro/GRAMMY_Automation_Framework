9f870d1b879fb805224b2e0caa11c7b82e58baa4
wagnermetaltoad <wagner.lima@metaltoad.com>
2022-10-14 13:34:36 -0300

    On going Refactor Automation Code - 10-14-2022

--- cypress/e2e/PRODUCTION/GRAMMY-PROD/SiteUpandRunningCheckin.js
+++ cypress/e2e/PRODUCTION/GRAMMY-PROD/SiteUpandRunningCheckin.js
@@ -21,21 +21,4 @@ describe('Validate SiteUp and Running in GRAMMY PROD', { tags: 'prod' }, functio
       })
     });
   })
-
-  describe('Validate SiteUp and Running in GRAMMY Museum Store PROD', { tags: 'prod' }, function () {
-    it('Opening URLs', function () {
-      cy.fixture('config.json').as('cfg');
-      cy.fixture('GrammyUrls').as('museumStore');
-      cy.get('@museumStore').then((json) => {
-        for (let i = 0; i < json.museumStore.length; i++) {
-          cy.visit(json.museumStore[i])
-          cy.wait(1000);
-          cy.log('Validation Complete')
-          cy.get('html:root').eq(0).should('not.have.value', 'Internal Server Error')
-          cy.get('html:root').eq(0).should('not.have.value', 'This page could not be found.')
-          cy.wait(500);
-        }
-      })
-    });
-  })
 })
