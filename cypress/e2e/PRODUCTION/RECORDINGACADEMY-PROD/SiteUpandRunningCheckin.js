1f9bbb16bc8d9d04e65b15fa3c87747f4d487fc3
wagnermetaltoad <wagner.lima@metaltoad.com>
2022-10-18 22:41:42 -0300

    Refactoring code and JSON files

--- cypress/e2e/PRODUCTION/RECORDINGACADEMY-PROD/SiteUpandRunningCheckin.js
+++ cypress/e2e/PRODUCTION/RECORDINGACADEMY-PROD/SiteUpandRunningCheckin.js
@@ -1,41 +1,23 @@
 /// <reference types="cypress-xpath" />
 
+var json = require('../../../fixtures/RecordAcademyUrls.json')
+var urls = Object.values(json.urls)
+
 describe('Validate SiteUp and Running in Record Academy PROD', { tags: 'prod' }, function () {
   beforeEach(function () {
     cy.viewport(1400, 900)
 
   })
-  describe('Validate SiteUp and Running in Record Academy PROD', { tags: 'prod' }, function () {
-    it('Opening URLs', function () {
-      cy.fixture('config.json').as('cfg');
-      cy.fixture('RecordAcademyUrls').as('urls');
-      cy.get('@urls').then((json) => {
-        for (let i = 0; i < json.urls.length; i++) {
-          cy.visit(json.urls[i])
-          cy.wait(1000);
-          cy.log('Validation Complete')
-          cy.get('html:root').eq(0).should('not.have.value', 'Internal Server Error')
-          cy.get('html:root').eq(0).should('not.have.value', 'This page could not be found.')
-          cy.wait(500);
-        }
-      })
+
+  urls.forEach(url => {
+    it(`Checking ${url}`, function () {
+      cy.visit(url)
+      cy.wait(1000);
+      cy.log('Validation Complete')
+      cy.get('html:root').eq(0).should('not.have.value', 'Internal Server Error')
+      cy.get('html:root').eq(0).should('not.have.value', 'This page could not be found.')
+      cy.wait(500);
     });
   })
 
-  describe('Validate SiteUp and Running in Record Academy Forms PROD', { tags: 'prod' }, function () {
-    it('Opening URLs', function () {
-      cy.fixture('config.json').as('cfg');
-      cy.fixture('RecordAcademyUrls').as('forms');
-      cy.get('@forms').then((json) => {
-        for (let i = 0; i < json.forms.length; i++) {
-          cy.visit(json.forms[i])
-          cy.wait(1000);
-          cy.log('Validation Complete')
-          cy.get('html:root').eq(0).should('not.have.value', 'Internal Server Error')
-          cy.get('html:root').eq(0).should('not.have.value', 'This page could not be found.')
-          cy.wait(500);
-        }
-      })
-    });
-  })
 })
